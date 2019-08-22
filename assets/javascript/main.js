024d74c8b97c0f8108996a3c97ebfd85845d1c30
marvin lara <laramarvin105@gmail.com>
2019-08-22 18:15:30 -0400

    updated

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,53 +3,46 @@
 
 var targetScore = 39;
 var counter= 0;
-var numberValue =[5, 10, 15, 1];
+
 var Crystals= ["rubyImage", "emeraldImage", "opalImage", "sapphireImage"];
 
+
 $("#number-to-guess").text(targetScore);
+var ruby = 5;
+var sapphire= 10;
+var emerald= 15;
+var opal =1;
+
+var scoreCount = [ruby, sapphire, emerald,opal]
+
+$("#ruby").on('click', function(){
+    console.log(ruby);
+})
 
 
+$("#sapphire").on('click', function(){
+    console.log(sapphire); 
+})
 
 
-for (var i=0; i < numberValue.length; i++){
-    targetScore = 39;
-     counter= 0;
-     numberValue =[5, 10, 15, 1];
-     Crystals= ["rubyImage", "emeraldImage", "opalImage", "sapphireImage"];
-     buttons = $("<button>");
-    
-    buttons.addClass("image-button image image-button-color");
-    buttons.attr("data-image",numberValue [i]);
-    buttons.text(numberValue[i]);
-    $("#buttons").append(buttons);
-    
+$("#emerald").on('click', function(){
+    console.log(emerald); 
+})
 
-  
-    console.log(buttons)
-}
- 
-$(".buttons").on("click,", function() {
-    var crystalColor = ($(this).attr("data-value"));
-    crystalColor = parseInt(crystalColor);
-    counter += crystalColor;
-    crystalColor.addClass (" crystal image-color");
-    crystalColor.text($(this).attr("data-image"));
-    $("#display").append(crystalColor);
 
-    console.log("data value",crystalColor)
+$("#opalImage").on('click', function(){
+    console.log(opal); 
+})
 
 
 
 
 
 
-    alert("New Score" + counter);
 
-    if (counter === targetScore){
-        alert("You win")
-    }
-    else if (counter >= targetScore){
-        alert("You lose");
-    }
 
-});
+
+
+
+
+
